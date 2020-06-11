64704cd110c67b006ce1503c9ed0aa652e143945
Mohit Bhasi <mohitbhasi1998@gmail.com>
2020-06-11 14:30:27 +0530

    autofocus onto input field added

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 docker volume create --name sharedVol
 docker run -d -v sharedVol:/shared m0bi5/mqtt:latest
 docker run -d -v sharedVol:/shared -p 5000:5000 m0bi5/flask:latest
+firefox --kiosk http://localhost:5000
